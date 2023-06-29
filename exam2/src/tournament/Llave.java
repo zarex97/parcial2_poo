28158837c71b959d35a90816301a4e12b6765ac0
zarex97 <isaac.diaz@davinci.edu.ar>
2023-06-29 10:28:22 -0300

    fix: erased unnecesary files

--- exam2/src/tournament/Llave.java
+++
@@ -1,9 +0,0 @@
-package tournament;
-
-import java.util.ArrayList;
-
-public class Llave extends Season {
-        public ArrayList<Team> getEquiposQueAvanzan() {
-                return getTeams();
-        }
-}
