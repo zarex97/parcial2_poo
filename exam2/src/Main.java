f5e2f332fd2a83a8a0dae7120544f121ecf7aa47
zarex97 <isaac.diaz@davinci.edu.ar>
2023-06-29 10:10:22 -0300

    feat: added shape, circle, square and rectangle

--- exam2/src/Main.java
+++ exam2/src/Main.java
@@ -4,73 +4,162 @@ import tournament.Team;
 
 import java.util.ArrayList;
 
-public class Main {
+
+abstract class Shape {
+
+    private String color;
+
+    private boolean filled;
+
+    private double area;
+
+    abstract public double getArea();
+
+    abstract public double getPerimeter();
+
+    public String toString() {
+        return "Shape[color=" + this.color + ",filled=" + this.filled + "]"
+    }
+
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+}
+
+ class Circle extends Shape {
+     private double radius;
+     public double getRadius() {
+         return this.radius;
+     }
+
+     public void setRadius(double radius) {
+         this.radius=radius;
+     }
+
+     public double getArea(){
+         return this.radius*(3.1416*3.1416);
+     };
+
+     public double getPerimeter() {
+         return this.radius*2*(3.1416);
+     };
+
+    public String toString() {
+        return "Circle[Shape[color=" + this.color + ",filled=" + this.filled + "],radius="+ this.radius+"]"
+    }
+
+
+}
+
+
+class Rectangle extends Shape{
+
+
+
+    private double width;
+
+    private double length;
+
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea(){
+        return getLength() * getWidth();
+    };
+
+    public double getPerimeter() {
+        return (getLength()*2) * (getWidth()*2);
+    };
+
+
+    public String toString() {
+        return "Rectangle[Shape[color=" + this.color + ",filled=" + this.filled + "],width="+ this.width+",length"+this.length+"]"
+    }
+
+
+
+}
+
+class Square extends Shape{
+
+
+
+    private double width;
+
+    private double length;
+
+    private double side;
+
+   // public double getArea();
+    // public double getPerimeter();
+
+
+    public double getSide() {
+        return this.side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+
+    public double getWidth() {
+        return getSide();
+    }
+
+    public double getLength() {
+        return getSide();
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+
+
+    public String toString() {
+        return "Rectangle[Shape[color=" + this.color + ",filled=" + this.filled + "],width="+ this.width+",length"+this.length+"]"
+    }
+
+
+
+}
+
+
+
+    public class Main {
     public static void main(String[] args) {
 
-        Team teamA = new Team("Equipo A",{"Messi", "DiMaria", "Isabel", "Isaac"});
 
-        Team teamB = new Team("Equipos B",{"Tomas", "Jogher", "Paco", "Franco"} );
-
-        Team teamC = new Team("Equipo C",{"Almada", "Dybala", "DePaul", "MacAllister"});
-
-        Team teamD = new Team("Equipo D", {"Pepe", "Lucio", "Mario", "Luigi"});
-
-        Team teamE = new Team("Equipo E",{"Ana", "Carlos", "Sofia", "Javier", "Javier","Mariana"});
-
-        Team teamF = new Team("Equipo F",{"Laura", "Diego", "Valentina", "Alejandro", "Gabriela"});
-
-
-        String [] playersG = {"Juancito","Roman","Cordoba","Clemente","Diaz"};
-        Team teamG = new Team("Equipo G", playersG);
-
-        String [] playersH = {"Aymar","Palermo","Cruz","Nacho","Ruben"};
-        Team teamH = new Team("Equipo H", playersH);
-
-       teamA.showMembers();
-      
-       teamB.showMembers();
-  
-        //System.out.println(tomas.showTeam());
-        ArrayList<Team> equiposCuarto= new ArrayList<Team>();
-        equiposCuarto.add(teamA);
-        equiposCuarto.add(teamB);
-        equiposCuarto.add(teamC);
-        equiposCuarto.add(teamD);
-        equiposCuarto.add(teamE);
-        equiposCuarto.add(teamF);
-        equiposCuarto.add(teamG);
-        equiposCuarto.add(teamH);
-
-
-        ArrayList<Match> partidosCuarto = new ArrayList<Match>();
-        partidosCuarto.add(new Match(equiposCuarto.get(0), equiposCuarto.get(1)));
-        partidosCuarto.add(new Match(equiposCuarto.get(2), equiposCuarto.get(3)));
-        partidosCuarto.add(new Match(equiposCuarto.get(4), equiposCuarto.get(5)));
-        partidosCuarto.add(new Match(equiposCuarto.get(6), equiposCuarto.get(7)));
-
-
-        //Rearrange games based on new Match structure (partidosCuartos)
-
-        //primerPartido.makeLocalGoal(1);
-        //primerPartido.makeVisitantGoal(2);
-        //primerPartido.makeVisitantGoal(3);
-        //System.out.println(primerPartido.getMatchStatus());
-
-        //segundoPartido.makeLocalGoal(0);
-        //System.out.println(segundoPartido.getMatchStatus());
-
-        //tercerPartido.makeVisitantGoal(1);
-        //tercerPartido.makeVisitantGoal(2);
-        //tercerPartido.makeLocalGoal(0);
-        //tercerPartido.makeLocalGoal(3);
-        //tercerPartido.makeLocalGoal(2);
-        //System.out.println(tercerPartido.getMatchStatus());
-
-        //cuartoPartido.makeLocalGoal(2);
-        //cuartoPartido.makeVisitantGoal(0);
-        //cuartoPartido.makeVisitantGoal(1);
-        //cuartoPartido.makeLocalGoal(3);
-        //System.out.println(cuartoPartido.getMatchStatus());
 
     }
 
